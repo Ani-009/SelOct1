f0e47b6583c0ad67703dbfc60ef2a9c4ca5e4a91
VIJUAPPU <VIJUAPPU@APPU-PC>
2019-10-01 12:13:40 -0400

    Source Code

---
+++ Test2/src/browser/LocatorByClass.java
@@ -0,0 +1,30 @@
+package com.test.browser;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LocatorByClass {
+
+	public static WebDriver driver = null;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Selenium_workspace\\TestSelenium\\Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		// driver.get("https://mets.cdc.gov/");
+		driver.get("https://www.gmail.com/");
+		// driver.close();
+		/**
+		 * Use ctrl +f to look for similar selectors in developer tools
+		 */
+
+		WebElement nextbtn = driver.findElement(By.className("span.RveJvd.snByac"));
+
+		nextbtn.click();
+
+	}
+}
---
+++ Test2/src/main/java/com/test/browser/LaunchBrowser.java
@@ -0,0 +1,19 @@
+package com.test.browser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+	public static WebDriver driver = null;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Selenium_workspace\\TestSelenium\\Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://mets.cdc.gov/");
+		// driver.close();
+	}
+
+}
